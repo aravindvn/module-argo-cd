9d2505919707d67aa7a9fb6fc58a37238f2d3588
aravindvn <dasan.aravind@gmail.com>
2023-02-18 23:43:13 -0600

    updated v1aplha to v1beta

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws-iam-authenticator"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
@@ -13,7 +13,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws-iam-authenticator"
       args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     }
